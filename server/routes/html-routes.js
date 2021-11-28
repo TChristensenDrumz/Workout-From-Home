46ac65bf7c2c8adf057bf1fb5897ae45400878e2
Terrapin Christensen <terrapinchristensen@gmail.com>
2021-11-28 18:13:21 -0500

    html routes

---
+++ server/routes/html-routes.js
@@ -0,0 +1,16 @@
+// Requiring path to so we can use relative routes to our HTML files
+const path = require("path");
+
+// Requiring our custom middleware for checking if a user is logged in
+const isAuthenticated = require("../config/middleware/isAuthenticated");
+
+module.exports = function (app) {
+  app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "../../public/pages/Landing/index.html"));
+  });
+
+  // 404 Page Error
+  app.get("*", function (req, res) {
+    res.redirect("/home");
+  });
+};
