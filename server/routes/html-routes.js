18daad7fa1adcb85031ceab1fd40c4117a437702
Terrapin Christensen <terrapinchristensen@gmail.com>
2021-11-28 21:24:50 -0500

    static file layout

--- server/routes/html-routes.js
+++ server/routes/html-routes.js
@@ -6,13 +6,11 @@ const isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function (app) {
   app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "../../public/pages/Landing/index.html"));
+    res.sendFile(path.join(__dirname, "../../public/pages/landing.html"));
   });
 
   // 404 Page Error
   app.get("*", function (req, res) {
-    res.sendFile(
-      path.join(__dirname, "../../public/pages/NotFound/index.html")
-    );
+    res.sendFile(path.join(__dirname, "../../public/pages/404.html"));
   });
 };
