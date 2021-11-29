5b6e22e5013a60df9ea9edd499a4949d548ae0da
Terrapin Christensen <terrapinchristensen@gmail.com>
2021-11-29 16:54:39 -0500

    signup page

--- server/routes/html-routes.js
+++ server/routes/html-routes.js
@@ -5,6 +5,10 @@ const path = require("path");
 const isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function (app) {
+  app.get("/signup", function (req, res) {
+    res.sendFile(path.join(__dirname, "../../public/pages/signup.html"));
+  });
+
   app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "../../public/pages/landing.html"));
   });
