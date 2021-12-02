cc2288f07acec5859fc2b248f64d80d01ff31edf
Terrapin Christensen <terrapinchristensen@gmail.com>
2021-12-02 14:20:49 -0500

    equipment array

--- server/models/user.js
+++ server/models/user.js
@@ -20,7 +20,7 @@ module.exports = function (sequelize, DataTypes) {
     equipment: {
       type: DataTypes.STRING,
       allowNull: true,
-      defaultValue: 7,
+      defaultValue: [],
     },
   });
   // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
