9f51112cb5c44c1305419e12e47978d922458649
Terrapin Christensen <terrapinchristensen@gmail.com>
2021-12-01 20:36:46 -0500

    login validation

---
+++ public/js/login.js
@@ -0,0 +1,82 @@
+$(document).ready(function () {
+  // Getting references to our form and input
+  const loginForm = $("form#login");
+  const emailInput = $("input#userEmail");
+  const passwordInput = $("input#userPassword");
+  const emailHelp = $("small#emailHelp");
+  const passwordError = $("small#passwordError");
+
+  // When the signup button is clicked, we validate the email and password are not blank
+  loginForm.on("submit", function (event) {
+    event.preventDefault();
+    const userData = {
+      email: emailInput.val().trim(),
+      password: passwordInput.val().trim(),
+    };
+
+    // Throw errors if information is missing
+    if (!userData.email) {
+      emailInput.attr("style", "border-color: red");
+      emailHelp.text("Please enter an email");
+      if (!userData.password) {
+        passwordInput.attr("style", "border-color: red");
+        passwordError.text("Please enter a password");
+      }
+      return;
+    }
+
+    emailHelp.text("");
+
+    if (!userData.password) {
+      passwordInput.attr("style", "border-color: red");
+      passwordError.text("Please enter a password");
+      return;
+    }
+
+    passwordError.text("");
+
+    // If we have an email and password, run the loginUser function
+    loginUser(userData.email, userData.password);
+    passwordInput.val("");
+  });
+
+  emailInput.on("click", function (event) {
+    event.preventDefault();
+    emailInput.removeAttr("style");
+  });
+
+  passwordInput.on("click", function (event) {
+    event.preventDefault();
+    passwordInput.removeAttr("style");
+  });
+
+  // Does a post to the login route. If successful, we are redirected to the members page
+  // Otherwise we log any errors
+  function loginUser(email, password) {
+    $.post("/api/login", {
+      email: email,
+      password: password,
+    })
+      .then(function (data) {
+        emailInput.val("");
+        location.replace("/home");
+      })
+      // If there's an error, handle it with the error function
+      .catch(handleLoginErr);
+  }
+
+  function handleLoginErr(err) {
+    console.log(err);
+    if (err.responseJSON?.type === "invalidEmail") {
+      emailInput.attr("style", "border-color: red");
+      emailHelp.text("Invalid email address");
+      return;
+    }
+    if (err.responseJSON?.fields?.email) {
+      emailInput.attr("style", "border-color: red");
+      emailHelp.text("An account with this email address already exists");
+      return;
+    }
+    alert(`Error code ${err.status}: ${err.statusText}`);
+  }
+});
