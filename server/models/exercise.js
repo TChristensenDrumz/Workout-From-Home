cc9867b2bff050ef59a39b6dbf5ec4942d31e12d
Terrapin Christensen <terrapinchristensen@gmail.com>
2021-12-07 21:28:47 -0500

    sql insert no timestamps

--- server/models/exercise.js
+++ server/models/exercise.js
@@ -1,18 +1,22 @@
 module.exports = function (sequelize, DataTypes) {
-  const Exercise = sequelize.define("Exercise", {
-    exercise_name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
+  const Exercise = sequelize.define(
+    "Exercise",
+    {
+      exercise_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      category: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      equipment: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
     },
-    category: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    equipment: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  });
+    { timestamps: false }
+  );
   return Exercise;
 };
