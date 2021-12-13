1017f0b2f48193d0fcf5117a203d50696ccf861d
Terrapin Christensen <terrapinchristensen@gmail.com>
2021-12-13 15:55:28 -0500

    routine page

--- server/routes/html-routes.js
+++ server/routes/html-routes.js
@@ -5,6 +5,10 @@ const path = require("path");
 const isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function (app) {
+  app.get("/routine", isAuthenticated, function (req, res) {
+    res.sendFile(path.join(__dirname, "../../public/pages/routine.html"));
+  });
+
   app.get("/gym", isAuthenticated, function (req, res) {
     res.sendFile(path.join(__dirname, "../../public/pages/gym.html"));
   });
